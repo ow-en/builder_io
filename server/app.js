7a21dfd5f817d36f38bd5372bfcc89847c1cf789
owensource <ciruz001@cougars.csusm.edu>
2018-07-07 15:40:55 -0700

    integrating mongodb

---
+++ server/app.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import next from 'next';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+mongoose.connect(MONGO_URL);
+
+const port = process.env.PORT || 8000;
+const ROOT_URL = process.env.ROOT_URL || `http://localhost:${port}`;
+
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/', (req, res) => {
+    const user = { email: 'team@builderbook.org' };
+    app.render(req, res, '/', { user });
+  });
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on ${ROOT_URL}`); // eslint-disable-line no-console
+  });
+});
