96486f4d145a01833781efbba3c564c56c3afb87
owensource <ciruz001@cougars.csusm.edu>
2018-07-08 23:30:16 -0700

    preparing for oauth integration-header component avatar

--- server/app.js
+++ server/app.js
@@ -1,7 +1,14 @@
 import dotenv from 'dotenv';
+
 import express from 'express';
 import next from 'next';
+
 import mongoose from 'mongoose';
+
+import session from 'express-session';
+import mongoSessionStore from 'connect-mongo';
+
+import User from './models/User';
 
 dotenv.config();
 
@@ -19,8 +26,28 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.get('/', (req, res) => {
-    const user = { email: 'team@builderbook.org' };
+  const MongoStore = mongoSessionStore(session);
+
+  const sess = {
+    name: 'book.sid',
+    secret: 'HD2w.)q*VqRT4/#NK2M/,E^B)}FED5fWU!dKe[wk',
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 14 * 24 * 60 * 60, // save session 14 days
+    }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: false,
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+    },
+  };
+
+  server.use(session(sess));
+
+  server.get('/', async (req, res) => {
+    req.session.foo = 'bar';
+    const user = await User.findOne({ slug: 'team-builder-book' });
     app.render(req, res, '/', { user });
   });
 
